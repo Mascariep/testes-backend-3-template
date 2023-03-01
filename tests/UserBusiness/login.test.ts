da9363a29586ce74ae040cdd65aa9f89af15b789
Mascariep <edipohc@gmail.com>
2023-03-01 11:46:12 -0300

    Exercícios de aula

--- tests/UserBusiness/login.test.ts
+++ tests/UserBusiness/login.test.ts
@@ -1,5 +1,7 @@
 import { UserBusiness } from "../../src/business/UserBusiness"
-import { LoginInputDTO } from "../../src/dtos/userDTO"
+import { LoginInputDTO, SignupInputDTO } from "../../src/dtos/userDTO"
+import { BadRequestError } from "../../src/errors/BadRequestError"
+import { NotFoundError } from "../../src/errors/NotFoundError"
 import { HashManagerMock } from "../mocks/HashManagerMock"
 import { IdGeneratorMock } from "../mocks/IdGeneratorMock"
 import { TokenManagerMock } from "../mocks/TokenManagerMock"
@@ -31,5 +33,54 @@ describe("login", () => {
 
         const response = await userBusiness.login(input)
         expect(response.token).toBe("token-mock-admin")
+    })
+
+    test("email não é uma string", async () => {
+        expect.assertions(1);
+
+        const input: LoginInputDTO = {
+            email: true,
+            password: "bananinha"
+        }
+
+        try {
+            
+            await userBusiness.login(input)
+
+        } catch (error) {
+            if (error instanceof BadRequestError){
+                expect(error.message).toBe("'email' deve ser string")
+            }    
+        }
+    })
+
+    test("Testar erro de email não cadastrado", async () => {
+        const input: LoginInputDTO = {
+            email: "naoexisto@email.com",
+            password: "bananinha"
+        }
+
+        expect(async () => {
+            await userBusiness.login(input)
+        }).rejects.toBeInstanceOf(NotFoundError) 
+    })
+
+    test("PASSWORD incorreto", async () => {
+        expect.assertions(1)
+
+        const input = {
+            email: "admin@email.com",
+            password: "password"
+        }
+
+        try {
+            
+            await userBusiness.login(input)
+
+        } catch (error) {
+            if (error instanceof BadRequestError){
+                expect(error.message).toBe("'password' incorreto")
+            }    
+        }
     })
 })
